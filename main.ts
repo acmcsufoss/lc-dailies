234a365d1e056ee2a57f7790cd90a6611dcb8f64
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-08-26 17:33:57 -0700

    WIP

--- main.ts
+++ main.ts
@@ -1,59 +1,36 @@
-import { WEBHOOK_URL } from "./env.ts";
+import * as env from "./env.ts";
 import * as discord from "./discord.ts";
-import * as lc from "./lc_client.ts";
+import * as lc from "./lc.ts";
 import * as snacks from "./snacks.ts";
-
-import type { LeaderboardClient } from "./leaderboard/mod.ts";
-import { DenoKvLeaderboardClient } from "./leaderboard/denokv/mod.ts";
 
 if (import.meta.main) {
   await main();
 }
 
-async function makeLeaderboardClient(): Promise<LeaderboardClient> {
-  return new DenoKvLeaderboardClient(
-    await Deno.openKv("./dev.db"),
-    new lc.LCClient(),
-  );
+async function main() {
+  const client = new lc.LCClient();
+  const question = await getDailyQuestion(client);
+  const content = formatLCDailyQuestion(question);
+  await discord.executeWebhook({
+    url: env.WEBHOOK_URL,
+    data: { content },
+  });
 }
 
-// TODO: Put everything back to normal.
-
-async function main() {
-  const client = await makeLeaderboardClient();
-  const result = await client.getCurrentSeason();
-  const result2 = await client.listSeasons();
-  // const result3 = await client.register(
-  //   "260901399729012736",
-  //   "EthanThatOneKid",
-  // );
-  // const result55 = await (await Deno.openKv("./dev.db"))
-  //   .get(["leaderboard_players", "260901399729012736"]);
-  const result4 = await client.submit(
-    "260901399729012736",
-    lc.parseSubmissionID(
-      "https://leetcode.com/problems/excel-sheet-column-title/submissions/1031839418/",
-    ),
+async function getDailyQuestion(
+  client: lc.LCClient,
+): Promise<lc.DailyQuestion> {
+  const date = new Date();
+  const [question] = await client.listDailyQuestions(
+    1,
+    date.getFullYear(),
+    date.getMonth() + 1,
   );
-  console.log({
-    result,
-    result2,
-    // result3,
-    // result55,
-    // result4,
-  });
+  if (!question) {
+    throw new Error("No daily question found");
+  }
 
-  // const date = new Date();
-  // const [question] = await client.listDailyQuestions(
-  //   1,
-  //   date.getFullYear(),
-  //   date.getMonth() + 1,
-  // );
-  // const content = formatLCDailyQuestion(question);
-  // await discord.executeWebhook({
-  //   url: WEBHOOK_URL,
-  //   data: { content },
-  // });
+  return question;
 }
 
 function formatLCDailyQuestion(question: lc.DailyQuestion): string {
