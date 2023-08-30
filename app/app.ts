91964915fe1141f4ed40067228adf21e95d211d4
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-08-29 20:23:43 -0700

    WIP

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,33 @@
-import type { RESTPostAPIApplicationCommandsJSONBody } from "../deps.ts";
-
-import { SUB_REGISTER } from "./sub/register.ts";
-import { SUB_SUBMIT } from "./sub/submit.ts";
+import type {
+  APIInteraction,
+  APIInteractionResponse,
+  APIInteractionResponseDeferredChannelMessageWithSource,
+  APIUser,
+  RESTPostAPIApplicationCommandsJSONBody,
+} from "../deps.ts";
+import {
+  ApplicationCommandOptionType,
+  InteractionResponseType,
+  InteractionType,
+  MessageFlags,
+  Utils,
+} from "../deps.ts";
+import * as leaderboard from "../leaderboard/mod.ts";
+import * as server from "../server/mod.ts";
+import * as discord from "../discord/mod.ts";
+import * as lc from "../lc/mod.ts";
+import {
+  makeRegisterInteractionResponse,
+  parseRegisterOptions,
+  REGISTER,
+  SUB_REGISTER,
+} from "./sub/register.ts";
+import {
+  makeSubmitInteractionResponse,
+  parseSubmitOptions,
+  SUB_SUBMIT,
+  SUBMIT,
+} from "./sub/submit.ts";
 
 export const LC = "lc";
 export const LC_DESCRIPTION =
@@ -15,3 +41,149 @@ export const APP_LC: RESTPostAPIApplicationCommandsJSONBody = {
   description: LC_DESCRIPTION,
   options: [SUB_REGISTER, SUB_SUBMIT],
 };
+
+/**
+ * makeDiscordAppHandler creates a handler for Discord application command interactions.
+ */
+export function makeDiscordAppHandler(
+  leaderboardClient: leaderboard.LeaderboardClient,
+  discordPublicKey: string,
+  discordChannelID: string,
+) {
+  return async function handleDiscordApp(
+    request: server.ServerRequest,
+  ): Promise<Response> {
+    // Verify the request is coming from Discord.
+    const { error, body } = await discord.verify(
+      request.request,
+      discordPublicKey,
+    );
+    if (error !== null) {
+      return error;
+    }
+
+    // Parse the incoming request as JSON.
+    const interaction = await JSON.parse(body) as APIInteraction;
+    switch (interaction.type) {
+      case InteractionType.Ping: {
+        return Response.json({ type: InteractionResponseType.Pong });
+      }
+
+      case InteractionType.ApplicationCommand: {
+        // Assert the interaction is a context menu interaction.
+        if (
+          !Utils.isChatInputApplicationCommandInteraction(interaction)
+        ) {
+          return new Response("Invalid request", { status: 400 });
+        }
+
+        // Assert the interaction is within the specified channel.
+        if (interaction.channel?.id !== discordChannelID) {
+          return new Response("Invalid request", { status: 400 });
+        }
+
+        // Assert the interaction is from a member.
+        if (!interaction.member?.user) {
+          return new Response("Invalid request", { status: 400 });
+        }
+
+        // Assert the interaction has options.
+        if (
+          !interaction.data.options || interaction.data.options.length === 0
+        ) {
+          throw new Error("No options provided");
+        }
+
+        // Assert the interaction has a subcommand.
+        const { 0: { name, type } } = interaction.data.options;
+        if (type !== ApplicationCommandOptionType.Subcommand) {
+          throw new Error("Invalid option type");
+        }
+
+        // Assert the interaction has a subcommand.
+        if (!interaction.member) {
+          throw new Error("No user provided");
+        }
+
+        // Handle the subcommand.
+        switch (name) {
+          case REGISTER: {
+            const handleRegisterSubcommand = makeRegisterSubcommandHandler(
+              leaderboardClient,
+            );
+            return Response.json(
+              await handleRegisterSubcommand(
+                interaction.member.user,
+                parseRegisterOptions(interaction.data.options),
+              ),
+            );
+          }
+
+          case SUBMIT: {
+            const handleSubmitSubcommand = makeSubmitSubcommandHandler(
+              leaderboardClient,
+            );
+            return Response.json(
+              await handleSubmitSubcommand(
+                interaction.member.user,
+                parseSubmitOptions(interaction.data.options),
+              ),
+            );
+          }
+        }
+
+        // Acknowledge the interaction.
+        return Response.json(
+          {
+            type: InteractionResponseType.DeferredChannelMessageWithSource,
+            data: {
+              flags: MessageFlags.Ephemeral,
+            },
+          } satisfies APIInteractionResponseDeferredChannelMessageWithSource,
+        );
+      }
+
+      default: {
+        return new Response("Invalid request", { status: 400 });
+      }
+    }
+  };
+
+  function makeRegisterSubcommandHandler(
+    leaderboardClient: leaderboard.LeaderboardClient,
+  ) {
+    /**
+     * handleRegisterSubcommand handles the register subcommand.
+     */
+    return async function handleRegisterSubcommand(
+      user: APIUser,
+      options: ReturnType<typeof parseRegisterOptions>,
+    ): Promise<APIInteractionResponse> {
+      const registerResponse = await leaderboardClient.register(
+        user.id,
+        options.lc_username,
+      );
+
+      return makeRegisterInteractionResponse(registerResponse);
+    };
+  }
+
+  function makeSubmitSubcommandHandler(
+    leaderboardClient: leaderboard.LeaderboardClient,
+  ) {
+    /**
+     * handleSubmitSubcommand handles the submit subcommand.
+     */
+    return async function handleSubmitSubcommand(
+      user: APIUser,
+      options: ReturnType<typeof parseSubmitOptions>,
+    ): Promise<APIInteractionResponse> {
+      const submitResponse = await leaderboardClient.submit(
+        user.id,
+        lc.parseSubmissionID(options.submission_url),
+      );
+
+      return makeSubmitInteractionResponse(submitResponse);
+    };
+  }
+}
---
+++ server/handlers/daily/daily.ts
@@ -0,0 +1,51 @@
+import * as discord from "../../../discord/mod.ts";
+import * as lc from "../../../lc/mod.ts";
+import * as server from "../../mod.ts";
+import * as snacks from "./snacks.ts";
+
+/**
+ * makeDailyWebhookPostHandler creates a handler for daily webhook POST requests.
+ */
+export function makeDailyWebhookPostHandler(
+  lcClient: lc.LCClient,
+  webhookURL: string,
+  webhookToken?: string,
+) {
+  /**
+   * handlePostDailyWebhook handles POST requests to the daily webhook.
+   */
+  return async function handlePostDailyWebhook(
+    request: server.ServerRequest,
+  ): Promise<Response> {
+    const token = request.params["token"];
+    if (webhookToken && token !== webhookToken) {
+      return new Response("Invalid token", { status: 401 });
+    }
+
+    return await handleExecuteDailyWebhook(lcClient, webhookURL);
+  };
+}
+
+async function handleExecuteDailyWebhook(
+  lcClient: lc.LCClient,
+  webhookURL: string,
+): Promise<Response> {
+  const question = await lcClient.getDailyQuestion();
+  const content = formatLCDailyQuestion(question);
+  await discord.executeWebhook({
+    url: webhookURL,
+    data: { content },
+  });
+
+  return new Response("OK");
+}
+
+function formatLCDailyQuestion(question: lc.DailyQuestion): string {
+  return [
+    `## Daily Leetcode Question for ${question.date}`,
+    `**Question**: ${question.title}`,
+    `**Difficulty**: ${question.difficulty}`,
+    `**Link**: <${question.url}>`,
+    `**Snack**: Here is a snack to get your brain working: ${snacks.pickRandom()}`,
+  ].join("\n");
+}
