50afdd2a330e2e610591b0bf667d2c0b7d57b5c8
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2023-09-03 23:50:40 -0700

    Refactor parseSubmissionID
    TODO: Add unit testing
    Co-Authored-By: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>

--- lib/lc/client.ts
+++ lib/lc/client.ts
@@ -206,13 +206,27 @@ async function gql(body: string): Promise<Response> {
  * parseSubmissionID parses the submission ID from the submission URL.
  */
 export function parseSubmissionID(submissionURLOrID: string): string {
-  if (LEETCODE_SUBMISSIONS_URL_PATTERN.test(submissionURLOrID)) {
-    return submissionURLOrID.replace(LEETCODE_SUBMISSIONS_URL_PATTERN, "")
-      .replace(/\/$/, "");
+  let submissionID = submissionURLOrID;
+  try {
+    const url = new URL(submissionURLOrID);
+    if (LEETCODE_SUBMISSIONS_PATHNAME_PATTERN.test(url.pathname)) {
+      submissionID = url.pathname
+        .replace(LEETCODE_SUBMISSIONS_PATHNAME_PATTERN, "")
+        .replace(/\/$/, "");
+    }
+  } catch {
+    // noop
   }
-
-  return submissionURLOrID;
+  return submissionID;
 }
 
-const LEETCODE_SUBMISSIONS_URL_PATTERN =
-  /^https:\/\/leetcode\.com\/(problems\/.*\/submissions\/|submissions\/detail\/)/;
+const LEETCODE_SUBMISSIONS_PATHNAME_PATTERN =
+  /^\/(problems\/.*\/submissions\/|submissions\/detail\/)/;
+
+/**
+ * Valid submission URLs: This entails a full URL or the direct submission ID.
+ * https://leetcode.com/problems/implement-stack-using-queues/submissions/1035629181/
+ * https://leetcode.com/submissions/detail/1035629181/
+ * 1035629181
+ * https://leetcode.com/problems/unique-paths/submissions/1039832006/?envType=daily-question&envId=2023-09-03
+ */
