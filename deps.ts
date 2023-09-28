ea62cac7ca1a1db505757287364715ca01e116ac
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-09-28 13:34:54 -0700

    run `deno task all`

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.201.0/assert/mod.ts";
+} from "https://deno.land/std@0.203.0/assert/mod.ts";
 
-export { load } from "https://deno.land/std@0.201.0/dotenv/mod.ts";
-export * from "https://deno.land/std@0.201.0/datetime/constants.ts";
+export { load } from "https://deno.land/std@0.203.0/dotenv/mod.ts";
+export * from "https://deno.land/std@0.203.0/datetime/constants.ts";
 export { ulid } from "https://deno.land/x/ulid@v0.3.0/mod.ts";
 export type {
   APIApplicationCommandInteractionDataOption,
@@ -16,12 +16,12 @@ export type {
   APIUser,
   RESTPostAPIApplicationCommandsJSONBody,
   RESTPostAPIWebhookWithTokenJSONBody,
-} from "https://deno.land/x/discord_api_types@0.37.56/v10.ts";
+} from "https://deno.land/x/discord_api_types@0.37.58/v10.ts";
 export {
   ApplicationCommandOptionType,
   InteractionResponseType,
   InteractionType,
   MessageFlags,
   Utils,
-} from "https://deno.land/x/discord_api_types@0.37.56/v10.ts";
+} from "https://deno.land/x/discord_api_types@0.37.58/v10.ts";
 export { default as nacl } from "https://cdn.skypack.dev/tweetnacl@1.0.3";
